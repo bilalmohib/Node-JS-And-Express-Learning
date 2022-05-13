f01bffe3f0e2921c0b5284a2073028e07af62146
Muhammad-Bilal-7896 <bilalmohib7896@gmail.com>
2022-05-13 15:51:24 +0500

    feat

---
+++ Node JS Tutorial/app.js
@@ -0,0 +1,11 @@
+// FreeCodeCamp Node JS tutorial
+
+const amount = 12
+
+if (amount < 10){
+    console.log("Small Number");
+}else {
+    console.log("Large number")
+}
+
+console.log(`Its my first Node App!!!`)
